6e45fd85be937bdca8e6e6687018b262591139ca
Aniruddhadey1992 <dey.aniruddha@yahoo.com>
2022-12-22 22:17:28 -0500

    Changes for FIbonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,6 +2,7 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        System.out.println("Hello World");
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
